04089898f0e7b0fa625bb5c9abf583b305ce0493
puskottampandey <puskottam.pandey@gmail.com>
2023-08-17 10:44:34 +0545

    Gridview iteam created

---
+++ lib/model/gridview_iteam_model.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ContainerProperties {
+  final IconData icon;
+  final Color iconColor;
+  final Color containerColor;
+  final String text;
+
+  ContainerProperties(
+      {required this.icon,
+      required this.iconColor,
+      required this.containerColor,
+      required this.text});
+}
+
+List<ContainerProperties> dataitems = [
+  ContainerProperties(
+      icon: Icons.apps,
+      iconColor: Colors.purple.shade100,
+      containerColor: const Color.fromARGB(80, 227, 193, 233),
+      text: "Apps"),
+  ContainerProperties(
+      icon: Icons.web_stories,
+      iconColor: Colors.pink.shade100,
+      containerColor: const Color.fromARGB(80, 255, 192, 203),
+      text: "Websites"),
+  ContainerProperties(
+      icon: Icons.design_services_outlined,
+      iconColor: Colors.yellow,
+      containerColor: const Color.fromARGB(80, 238, 226, 123),
+      text: "Designs"),
+  ContainerProperties(
+      icon: Icons.person,
+      iconColor: Colors.cyan.shade100,
+      containerColor: const Color.fromARGB(200, 245, 245, 245),
+      text: "Consultant"),
+];
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+import '../model/gridview_iteam_model.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -36,49 +38,121 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: const SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Hello,Suvaye Tech",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Service",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Hello,Suvaye Tech",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        "See more",
-                        style: TextStyle(
-                            color: Colors.green, fontWeight: FontWeight.bold),
-                      ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.green,
-                        size: 15,
-                      )
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-            ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Service",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "See more",
+                          style: TextStyle(
+                              color: Colors.green, fontWeight: FontWeight.bold),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.green,
+                          size: 15,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 200,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Outline",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "See more",
+                          style: TextStyle(
+                              color: Colors.green, fontWeight: FontWeight.bold),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.green,
+                          size: 15,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                mainAxisSpacing: 30,
+                                crossAxisSpacing: 30,
+                                mainAxisExtent: 60,
+                                crossAxisCount: 2),
+                        itemCount: dataitems.length,
+                        itemBuilder: (context, index) {
+                          final data = dataitems[index];
+
+                          return Container(
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: data.containerColor,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Icon(
+                                    data.icon,
+                                    color: data.iconColor,
+                                  ),
+                                  Text(
+                                    data.text,
+                                    style: const TextStyle(letterSpacing: 0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
