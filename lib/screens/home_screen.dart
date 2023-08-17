0b28b99729357fc4c245973c908d625cbf8c9703
puskottampandey <puskottam.pandey@gmail.com>
2023-08-17 12:56:58 +0545

    bottom navigationbar created

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
-import '../model/gridview_iteam_model.dart';
+import 'package:suvayeproject/screens/Services_screen.dart';
+import 'package:suvayeproject/screens/chat_screen.dart';
+import 'package:suvayeproject/screens/today_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +11,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selected = 0;
+
+  Widget widgetScreen(int index) {
+    switch (index) {
+      case 0:
+        return const TodayScreen();
+      case 1:
+        return const ServiceScreen();
+      case 2:
+        return const ChatScreen();
+    }
+    return const Text("not found");
+  }
+
+  void onTap(int index) {
+    setState(() {
+      selected = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,124 +57,18 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
-        child: SingleChildScrollView(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Hello,Suvaye Tech",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Service",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "See more",
-                          style: TextStyle(
-                              color: Colors.green, fontWeight: FontWeight.bold),
-                        ),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.green,
-                          size: 15,
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 200,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Outline",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "See more",
-                          style: TextStyle(
-                              color: Colors.green, fontWeight: FontWeight.bold),
-                        ),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.green,
-                          size: 15,
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: GridView.builder(
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                                mainAxisSpacing: 30,
-                                crossAxisSpacing: 30,
-                                mainAxisExtent: 60,
-                                crossAxisCount: 2),
-                        itemCount: dataitems.length,
-                        itemBuilder: (context, index) {
-                          final data = dataitems[index];
-
-                          return Container(
-                            width: 50,
-                            decoration: BoxDecoration(
-                              color: data.containerColor,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Icon(
-                                    data.icon,
-                                    color: data.iconColor,
-                                  ),
-                                  Text(
-                                    data.text,
-                                    style: const TextStyle(letterSpacing: 0),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        }),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: widgetScreen(selected),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selected,
+          onTap: onTap,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today_outlined), label: 'Today'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.apps_outlined), label: "Servies"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.chat_bubble_outline), label: "Chat"),
+          ]),
     );
   }
 }
